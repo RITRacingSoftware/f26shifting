695281b334855bcc2c15224924af11745b176f93
CRP5488 <crp5488@rit.edu>
2022-10-27 22:08:01 -0400

    Added so that paddle has to be pressed for 10 ms before shift

--- f26_shifting.ino
+++ f26_shifting.ino
@@ -6,12 +6,18 @@
 #define UPSHIFT_SOLENOID_SIG 4 //Should be correct pin
 #define DOWNSHIFT_SOLENOID_SIG 7 //Should be correct pin    
 
+// How long a paddle has to be pressed to register and shift
+#define BUTTON_HOLD_TIME_MS 10
+
 // How long to hold the solenoid signal low for
-#define SOLENOID_SIGNAL_TIME 10
+#define SOLENOID_SIGNAL_TIME_MS 10
 
 bool upshift_paddle_still_pressed = false;
 //bool downshift_button_still_pressed = false;
 bool downshift_paddle_still_pressed = false;
+
+int upshift_paddle_pressed_time = 0;
+int downshift_paddle_pressed_time = 0;
 
 void setup() {
   pinMode(UPSHIFT_PADDLE_PIN, INPUT_PULLUP);
@@ -30,7 +36,7 @@ void setup() {
 void upshift(void)
 {
   digitalWrite(UPSHIFT_SOLENOID_SIG, LOW);
-  delay(SOLENOID_SIGNAL_TIME);
+  delay(SOLENOID_SIGNAL_TIME_MS);
   digitalWrite(UPSHIFT_SOLENOID_SIG, HIGH);
 }
 
@@ -38,7 +44,7 @@ void upshift(void)
 void downshift(void)
 {
   digitalWrite(DOWNSHIFT_SOLENOID_SIG, LOW);
-  delay(SOLENOID_SIGNAL_TIME);
+  delay(SOLENOID_SIGNAL_TIME_MS);
   digitalWrite(DOWNSHIFT_SOLENOID_SIG, HIGH);
 }
 
@@ -47,8 +53,13 @@ void loop() {
   // Upshift if paddle is presed and hasn't shifted already for the press
   if((!upshift_paddle_still_pressed) && (digitalRead(UPSHIFT_PADDLE_PIN) == LOW))
   {
-    upshift();
-    upshift_paddle_still_pressed = true;
+    // Only shift if the paddle is held for BUTTON_HOLD_TIME_MS milliseconds
+    delay(BUTTON_HOLD_TIME_MS);
+    if(digitalRead(UPSHIFT_PADDLE_PIN) == LOW)
+    {
+      upshift();
+      upshift_paddle_still_pressed = true;
+    }
   }
   else if(digitalRead(UPSHIFT_PADDLE_PIN) == HIGH)
   {
@@ -69,8 +80,13 @@ void loop() {
   // Downshift if paddle is presed and hasn't shifted already for the press
   if((!upshift_paddle_still_pressed) && (digitalRead(DOWNSHIFT_PADDLE_PIN) == LOW))
   {
-    downshift();
-    downshift_paddle_still_pressed = true;
+    // Only shift if the paddle is held for BUTTON_HOLD_TIME_MS milliseconds
+    delay(BUTTON_HOLD_TIME_MS);
+    if(digitalRead(DOWNSHIFT_PADDLE_PIN) == LOW)
+    {
+      downshift();
+      downshift_paddle_still_pressed = true;
+    }
   }
   else if(digitalRead(DOWNSHIFT_PADDLE_PIN) == HIGH)
   {
